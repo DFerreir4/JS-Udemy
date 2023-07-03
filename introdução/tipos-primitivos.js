94fb97bb6f5b428d6da43a6c007aebf7e0633b65
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-03 20:39:09 -0300

    undefined, null

--- introdução/tipos-primitivos.js
+++ introdução/tipos-primitivos.js
@@ -1,3 +1,5 @@
+// Os tipos de dados => Strings, Number(int e float) , Boolean
+
 let minha_var = "minha string"
 let minha_var2 = 'minha string'
 console.log(minha_var2)
@@ -19,3 +21,12 @@ console.log(`o tipo de n2 é ${typeof n2} e o seu valor é ${n2}`)
 
 const isValid = true
 console.log(`IsValid: ${isValid}`)
+
+//undefined, null , symbol[mais pra frent!] (ES2015)
+
+let varTeste = null
+console.log(varTeste)
+console.log(typeof varTeste)
+varTeste = 10
+console.log(typeof varTeste, varTeste)
+
