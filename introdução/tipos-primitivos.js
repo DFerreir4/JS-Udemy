5060cb3d1a3c206c959398eef1ca24c8e4f5bff5
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-03 20:25:39 -0300

    tipos primitivos number

--- introdução/tipos-primitivos.js
+++ introdução/tipos-primitivos.js
@@ -4,8 +4,15 @@ console.log(minha_var2)
 
 let idade = 40
 let msg = "Eu possuo "+idade+" anos. (concatenação)"
-let msg2 = `Eu possuo ${idade} anos.`
+let msg2 = `(Eu possuo ${idade} anos.)`
 
 console.log(msg)
 
 console.log(typeof msg, typeof idade , typeof minha_var)
+
+const n1 = 10
+const n2 = 1.1
+
+console.log(`o tipo de n1 é ${typeof n1} e o seu valor é ${n1}`)
+console.log(`o tipo de n2 é ${typeof n2} e o seu valor é ${n2}`)
+
