86cf795e27fc937afbd1e6fe1c4ba61a1a1dfcc4
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-03 20:12:43 -0300

    tipos de dados primitivos

---
+++ introdução/tipos-primitivos.js
@@ -0,0 +1,9 @@
+let minha_var = "minha string"
+let minha_var2 = 'minha string'
+console.log(minha_var2)
+
+let idade = 40
+let msg = "Eu possuo "+idade+" anos. (concatenação)"
+let msg2 = `Eu possuo ${idade} anos.`
+
+console.log(msg)
