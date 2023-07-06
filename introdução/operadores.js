851d468bd2bdab830e994374e9646ca6a45e169e
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-06 20:08:13 -0300

    operadores aritméticos parte 1

---
+++ introdução/operadores.js
@@ -0,0 +1,22 @@
+/*
+
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão
+% Resto da divisão
+** Potência
+
+*/
+
+let n1 = 10
+let n2 = 5
+
+console.log(n1 + n2)
+console.log( n1 - n2)
+console.log( n1 * n2)
+console.log( n1 / n2)
+console.log( n1 % n2)
+console.log( 2 ** 3)
+
+
