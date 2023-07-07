85c2dd90c5cdc14f29e18c057dfc454403ffeebf
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-07 10:15:50 -0300

    operadores de atribuição

--- introdução/operadores.js
+++ introdução/operadores.js
@@ -11,12 +11,20 @@
 
 let n1 = 10
 let n2 = 5
-
+//OPERADORES ARITMÉTICOS
 console.log(n1 + n2)
 console.log( n1 - n2)
 console.log( n1 * n2)
 console.log( n1 / n2)
 console.log( n1 % n2)
 console.log( 2 ** 3)
+
+//OPERADORES DE ATRIBUIÇÃO
+
+let n3 = 20
+//n3 = n3 + 15
+n3 *= 2
+console.log(`O valor de n3 é ${n3}`)
+
 
 
