8124a3028dc5bd83074d3dff8a02611733859ace
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-07 17:31:49 -0300

    if ternário

--- introdução/condicional-if-else.js
+++ introdução/condicional-if-else.js
@@ -1,20 +1,29 @@
-let idade = 4
-let paisPresentes = true
+let idade = 27
+let paisPresentes = false
 let compBilhete = true
 const podeViajar = (idade >= 18 || paisPresentes) && compBilhete
 
-console.log(podeViajar)
+console.log(podeViajar) // resultado da comparação
+
+
+let msgMaiorIdade = (idade >= 18) ? "É Maior de idade" : "É menor de 18 anos"
+
 
 if (!compBilhete) {
     console.log("Não! não comprou bilhete!")
     
     } else {
-        if(idade >= 18){
-            console.log("É Maior de idade, pode viajar!")
-        }else{
-            console.log("Opa. É menor de idade")
-        }
+        console.log(msgMaiorIdade)
     }
+
+//let msgMaiorIdade = ""
+// if(idade >= 18){
+//     msgMaiorIdade = "É maior de idade"
+// }else {
+//     msgMaiorIdade = "É menor de 18 anos"
+// } Operador ternário
+
+
 
 let n1 = 10
 let n2 = 1
@@ -28,3 +37,5 @@ let media = ( n1 + n2 ) / 2
             console.log("RECUPERAÇÃO")
         }
     
+
+      
