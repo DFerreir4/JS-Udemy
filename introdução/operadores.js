c0dd6795da0e6defb4af7a5d432395a17a2b85f0
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-07 15:12:45 -0300

    questão de lógica

--- introdução/operadores.js
+++ introdução/operadores.js
@@ -52,3 +52,23 @@ console.log(n2 <= 11)
 console.log("testando")
 console.log(n1 != "10") //false
 console.log(n1 !== "10") //true
+
+//Operadores Lógicos
+
+/*
+Para uma pessoa viajar para o exterior:
+- precisa ser maior de  18 anos ou acompanhando com os pais e ter comprado o bilhete.
+*/
+
+// let idade = 18
+// if(idade >= 18){
+//     console.log(`esta pessoa tem a idade ${idade}! Ela pode viajar sozinha.`)
+// } else {
+//     console.log(`esta pessoa tem a idade ${idade}, ela só viaja acompanhada com os pais.`)
+// }
+
+let idade = 21
+let paisPresentes = false
+let compBilhete = false
+const podeViajar = idade >= 18 || paisPresentes && compBilhete
+console.log(`pode viajar: ${podeViajar}`)
