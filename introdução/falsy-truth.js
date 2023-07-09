153ac46a65226a7274050028a18e553b027873bf
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-09 17:29:22 -0300

    falsy truth

---
+++ introdução/falsy-truth.js
@@ -0,0 +1,9 @@
+// valores falsy -> 0, "", NaN, undefined, null, false
+// truth -> todos os demais.
+
+
+if("false"){ //qualquer string != " "
+    console.log("true")
+} else {
+    console.log("false")
+}
