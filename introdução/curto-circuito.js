4725aa38d3a1999c6dac3c866c9a338a61f7b72f
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-09 17:52:10 -0300

    curto circuito

---
+++ introdução/curto-circuito.js
@@ -0,0 +1,16 @@
+let n = 1
+
+n = n || 10
+
+
+console.log(n)
+
+ let isValid = true
+// if(isValid){
+//     console.log("é valido")
+// }
+
+//isValid && console.log("Valido")
+
+isValid || console.log("Valido")
+
