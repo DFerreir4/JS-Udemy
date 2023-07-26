25ab7f08319fe37aa0402912a128f540bf860264
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-07-25 22:26:42 -0300

    arrays de objetos

---
+++ array-object.js
@@ -0,0 +1,27 @@
+const nomes = ["Daniel","Maria", ""]
+
+const pessoa1 = {
+    nome: "Daniel",
+    idade: 10,
+    email: ""
+}
+
+const pessoas = [{
+    nome: "Daniel",
+    idade: 40
+
+},{
+    nome: "Maria",
+    idade: 28
+
+},{
+    nome: "Helena",
+    idade: 45
+},{
+    nome: "João",
+    idade: 18
+
+
+}]
+
+console.log(pessoas[0].nome)
