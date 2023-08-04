16e86d2fe2ab6550573d7e8cf6d62776062a7206
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-04 16:14:43 -0300

    sistema léxico parte 1

--- introdução/primeiro-exemplo.js
+++ introdução/primeiro-exemplo.js
@@ -1,1 +1,28 @@
-console.log('Javascript funcionando! 222')
+//sistema léxico
+console.log("JavaScript funcionando 2222")
+
+let nome = "Diego"
+console.log(10 + 20)
+
+// Uma unica linha
+/* Multiplas linhas */
+
+//case sensitive
+
+let string = "Maçã"
+console.log(String)
+
+//palavras reservadas
+
+//ponto e virgula. Opcional a não ser que tenha duas instruções na mesma linha
+
+string = "banana"; alert(string)
+
+//como nomear váriaveis e funções
+
+function getNome(){//funções normais getNome - Construtoras ou classe GetNome
+    
+}
+
+//mesma coisa com váriaveis.
+//utilização de CamelCase - nomeDeFamilia
