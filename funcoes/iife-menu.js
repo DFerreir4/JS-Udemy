cf6f135e113dd64793e66be927ca1f9c54caae73
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-09 22:42:00 -0300

    parametros para função auto invocável

--- funcoes/iife-menu.js
+++ funcoes/iife-menu.js
@@ -5,12 +5,12 @@
 
 // init()
 
-(function(){
+(function(n1,n2,n3){
     let isValid = false
-    console.log("init Menu",isValid)
+    console.log("init Menu",isValid,n1,n2,n3)
 
     function init(){
         console.log("init do menu")
     }
     init()
-})()
+})(10,25,38)
--- funcoes/iife-modal.js
+++ funcoes/iife-modal.js
@@ -5,12 +5,12 @@
 
 // init()
 
-(function(){
+(function(n1,n2,n3){
     let isValid = true
-    console.log("init Modal",isValid)
+    console.log("init Modal",isValid,n1,n2,n3)
 
     function init(){
         console.log("init do Modal")
     }
     init()
-})()
+})(1,2,3)
