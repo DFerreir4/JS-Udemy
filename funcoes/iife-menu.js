2ab0746106cb9955d296b0fc6ecd8db52e69d020
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-09 22:37:51 -0300

    iife

---
+++ funcoes/iife-menu.js
@@ -0,0 +1,16 @@
+// function init(){
+//     var isValid = false
+//     console.log("init Menu",isValid)
+// }
+
+// init()
+
+(function(){
+    let isValid = false
+    console.log("init Menu",isValid)
+
+    function init(){
+        console.log("init do menu")
+    }
+    init()
+})()
---
+++ funcoes/iife-modal.js
@@ -0,0 +1,16 @@
+// function init(){
+//     var isValid = true
+//     console.log("init Modal",isValid)
+// }
+
+// init()
+
+(function(){
+    let isValid = true
+    console.log("init Modal",isValid)
+
+    function init(){
+        console.log("init do Modal")
+    }
+    init()
+})()
