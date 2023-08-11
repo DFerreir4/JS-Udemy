42f0a6245e4dde652a59c79aec46b47a086d3f0d
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-10 23:51:16 -0300

    arguments vs arrow function

--- funcoes/arguments.js
+++ funcoes/arguments.js
@@ -9,7 +9,37 @@
 // }
 // console.log(somar([1,10, 3]))
 
-function somar(){
+// function somar(){
+//     console.log(arguments)
+//     let total = 0
+//     for(let i = 0; i < arguments.length; i++){
+//         total += arguments[i]
+//     }
+//     return total
+// }
+
+// console.log(somar(1,2,3))
+// console.log(somar(1,2,3,4,5,6,))
+// console.log(somar(1,2,3,5,6,4,8,9))
+
+//alterando para function declaration
+
+// const somar = function () {
+//     console.log(arguments)
+//     let total = 0
+//     for(let i = 0; i < arguments.length; i++){
+//         total += arguments[i]
+//     }
+//     return total
+// }
+
+// console.log(somar(1,2,3))
+// console.log(somar(1,2,3,4,5,6,))
+// console.log(somar(1,2,3,5,6,4,8,9))
+
+//O MESMO NÃO VAI ACONTECER SE TIVER UMA ARROW FUNCTION
+
+const somar = () => {
     console.log(arguments)
     let total = 0
     for(let i = 0; i < arguments.length; i++){
