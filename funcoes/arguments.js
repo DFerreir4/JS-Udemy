8d05f111441840ee33f61b4eebefb2ba2d7b56d5
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-14 23:15:46 -0300

    propriedade name de function

--- funcoes/arguments.js
+++ funcoes/arguments.js
@@ -24,22 +24,7 @@
 
 //alterando para function declaration
 
-// const somar = function () {
-//     console.log(arguments)
-//     let total = 0
-//     for(let i = 0; i < arguments.length; i++){
-//         total += arguments[i]
-//     }
-//     return total
-// }
-
-// console.log(somar(1,2,3))
-// console.log(somar(1,2,3,4,5,6,))
-// console.log(somar(1,2,3,5,6,4,8,9))
-
-//O MESMO NÃO VAI ACONTECER SE TIVER UMA ARROW FUNCTION
-
-const somar = () => {
+const somar = function () {
     console.log(arguments)
     let total = 0
     for(let i = 0; i < arguments.length; i++){
@@ -51,3 +36,19 @@ const somar = () => {
 console.log(somar(1,2,3))
 console.log(somar(1,2,3,4,5,6,))
 console.log(somar(1,2,3,5,6,4,8,9))
+console.log(somar.name)
+
+//O MESMO NÃO VAI ACONTECER SE TIVER UMA ARROW FUNCTION
+
+// const somar = () => {
+//     console.log(arguments)
+//     let total = 0
+//     for(let i = 0; i < arguments.length; i++){
+//         total += arguments[i]
+//     }
+//     return total
+// }
+
+// console.log(somar(1,2,3))
+// console.log(somar(1,2,3,4,5,6,))
+// console.log(somar(1,2,3,5,6,4,8,9))
