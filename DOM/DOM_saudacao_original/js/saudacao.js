b0afdd9ff9d4faa56712e689437bdc24949023a6
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-23 17:24:01 -0300

    innerHTML

--- DOM/DOM_saudacao_original/js/saudacao.js
+++ DOM/DOM_saudacao_original/js/saudacao.js
@@ -4,7 +4,8 @@
    // document.querySelector("div.top-bar").textContent = `Bem-vindo(a), ${nomeUsuario}`  
     const nomeUsua = document.querySelector("div.top-bar")
     //nomeUsua.textContent = nomeUsua.textContent + nomeUsuario
-    nomeUsua.textContent += nomeUsuario
+   // nomeUsua.textContent += nomeUsuario
+   nomeUsua.innerHTML += `<em>${nomeUsuario}</em>`
     
 
 })()
