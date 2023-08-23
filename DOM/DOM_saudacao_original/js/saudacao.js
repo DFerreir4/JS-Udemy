5cbaf21e17dc6636cc1f9c3f37d1724fa2f8df01
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-23 15:59:47 -0300

    nome de usuário

--- DOM/DOM_saudacao_original/js/saudacao.js
+++ DOM/DOM_saudacao_original/js/saudacao.js
@@ -1,7 +1,10 @@
 //função autoinvocavel porque esta trabalhando com browser
 (function(){
     const nomeUsuario = "Diego"
-    document.querySelector("div.top-bar").textContent = `Bem-vindo(a), ${nomeUsuario}`  
+   // document.querySelector("div.top-bar").textContent = `Bem-vindo(a), ${nomeUsuario}`  
+    const nomeUsua = document.querySelector("div.top-bar")
+    //nomeUsua.textContent = nomeUsua.textContent + nomeUsuario
+    nomeUsua.textContent += nomeUsuario
     
 
 })()
