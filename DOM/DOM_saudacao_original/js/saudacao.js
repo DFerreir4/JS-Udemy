3481bfec5708bda8fe09dc1955d6c3bd6c1dd9f1
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-23 17:45:28 -0300

    esconder elemento

--- DOM/DOM_saudacao_original/js/saudacao.js
+++ DOM/DOM_saudacao_original/js/saudacao.js
@@ -1,11 +1,17 @@
 //função autoinvocavel porque esta trabalhando com browser
 (function(){
-    const nomeUsuario = "Diego"
-   // document.querySelector("div.top-bar").textContent = `Bem-vindo(a), ${nomeUsuario}`  
-    const nomeUsua = document.querySelector("div.top-bar")
-    //nomeUsua.textContent = nomeUsua.textContent + nomeUsuario
-   // nomeUsua.textContent += nomeUsuario
-   nomeUsua.innerHTML += `<em>${nomeUsuario}</em>`
+    const nomeUsuario = null
+    const nomeUsua = document.querySelector("div.top-bar p")
+
+    if(nomeUsuario){
+        // document.querySelector("div.top-bar").textContent = `Bem-vin     (a), ${nomeUsuario}`  
+        //nomeUsua.textContent = nomeUsua.textContent + nomeUsuario
+        // nomeUsua.textContent += nomeUsuario
+        nomeUsua.innerHTML += `<b>${nomeUsuario}</b>`
+    } else {
+        nomeUsua.parentElement.style.display = "none"
+    }
+   
     
 
 })()
