9b8a89ec70fa72c245a64c4898f06e8d2188af17
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-08-23 15:52:20 -0300

    mudando nome de usuário

---
+++ DOM/DOM_saudacao_original/js/saudacao.js
@@ -0,0 +1,7 @@
+//função autoinvocavel porque esta trabalhando com browser
+(function(){
+    const nomeUsuario = "Diego"
+    document.querySelector("div.top-bar").textContent = `Bem-vindo(a), ${nomeUsuario}`  
+    
+
+})()
