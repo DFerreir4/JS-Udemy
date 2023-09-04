7c28ca231ac8795824c79b4c5fe5561cb0aae6e1
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-09-04 16:30:43 -0300

    pop()

--- array/array04.js
+++ array/array04.js
@@ -4,3 +4,9 @@ let teste = arr.push(11,13,true,"Olá Mundo!")
 
 console.log(teste)// saiu 9 porque mostra a quantidade de índices no array
 console.log(arr)
+console.log("-----------")
+//let ultimoItem = arr.pop()
+let ultimoItem = arr[arr.length - 1]
+console.log(ultimoItem)
+console.log(arr)
+
