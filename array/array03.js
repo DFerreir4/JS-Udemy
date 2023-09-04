b3e9ce5dcfa3b3569352da2949fcf70d208d5870
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-09-04 15:40:55 -0300

    concat()
    serve para juntar valores

--- array/array03.js
+++ array/array03.js
@@ -3,3 +3,9 @@ let arr2 = [5,6,7]
 
 console.log(arr1.toString())
 console.log(arr1.join(" - "))
+let arr3 = arr1.concat(arr2, 4, 8, 9 , 10, ["Olá", " Mundo!"])
+
+
+console.log(arr1)
+console.log(arr2)
+console.log(arr3)
