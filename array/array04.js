7f7b267e13165883adda2083ed873ac2fbca75c6
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-09-04 15:57:15 -0300

    push()
    sempre vai alterar o array original e vai retornar o novo índice.

--- array/array04.js
+++ array/array04.js
@@ -1,5 +1,6 @@
 let arr = [1,3,5,7,9]
 
-arr.push(11,13,true,"Olá Mundo!")
+let teste = arr.push(11,13,true,"Olá Mundo!")
 
+console.log(teste)// saiu 9 porque mostra a quantidade de índices no array
 console.log(arr)
